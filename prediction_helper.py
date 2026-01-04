798eeaf13ecf689e3f5d69ed5049ad262585a9eb
joshisoham2 <56852609+joshisoham2@users.noreply.github.com>
2026-01-04 14:05:23 -0500

    Delete prediction_helper.py

--- prediction_helper.py
+++
@@ -1,9 +0,0 @@
-import joblib
-
-model = joblib.load("gb_model.pkl")
-
-def predict(input_df): 
-    prediction = model.predict(input_df)
-    prediction = str(round(prediction.item(), 2))
-    print(type(prediction))
-    return prediction
